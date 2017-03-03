98b80b3ad6302246763221a7fd7739582cb14260
Martin Guevara <solidsnake093@gmail.com>
2017-03-02 20:27:40 -0500

    esta funcion multiplica

--- sumar.php
+++ sumar.php
@@ -1,12 +1,12 @@
 <?php
 
-	function sumar (int $numero1,int $numero2 ):int 
+	function multiplicar (int $numero1,int $numero2 ):int 
 	{
-		return $numero1+$numero2;
+		return $numero1*$numero2;
 
 
 	}
 
-	echo "La suma es : ".sumar(5,6)." gracias por sumar con nosotros";
+	echo "La multiplicacion es : ".multiplicar(5,6)." gracias por multiplicar con nosotros";
 
 ?>
