9cc95135873ca1f82754a788d575dd60e3bdeb25
Martin Guevara <solidsnake093@gmail.com>
2017-03-02 20:23:39 -0500

    se agrego mas lineas a la suma

--- sumar.php
+++ sumar.php
@@ -2,12 +2,11 @@
 
 	function sumar (int $numero1,int $numero2 ):int 
 	{
-
 		return $numero1+$numero2;
 
 
 	}
 
-	echo "La suma es : ".sumar(5,6);
+	echo "La suma es : ".sumar(5,6)." gracias por sumar con nosotros";
 
 ?>
